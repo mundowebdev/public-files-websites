80b53879d864eb2da42720ab4205cee9c996782d
Regata OS Project <regataosproject@gmail.com>
2022-11-18 13:50:59 -0300

    Initial code to list menu items.

---
+++ pizzaria-lopes/javascript/list-menu-items.js
@@ -0,0 +1,27 @@
+const url = 'https://raw.githubusercontent.com/mundowebdev/public-files-websites/main/pizzaria-lopes/json/menu-pizza-list.json';
+
+fetch(url)
+    .then((response) => response.json())
+    .then((data) => {
+
+        data.map((data) => {
+            const getCardapioMain = document.querySelector(".cardapio-block-main");
+            const newElement = document.createElement('div');
+            newElement.classList.add('cardapio-block');
+
+            newElement.innerHTML = `
+                <img src="${data.img}" alt="Pizza de ${data.name}" />
+                <h3>${data.name}</h3>
+                <p class="item-desc">${data.desc}</p>
+                <div class="item-tipo">
+                    <p class="tamanho">Tamanho: Grande</p>
+                    <p class="preco">R$ ${data.price.large}</p>
+                </div>`;
+
+            getCardapioMain.appendChild(newElement);
+        })
+
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
